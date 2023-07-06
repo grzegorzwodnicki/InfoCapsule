a39294c2bef213677801852cbab0a340924fe885
Grzegorz Wodnicki <info@funchips.co.uk>
2023-07-06 13:44:55 +0100

    created news provider model

--- infocapsule/models.py
+++ infocapsule/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class NewsProvider(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    main_url = models.CharField(max_length=500, blank=False)
+    
